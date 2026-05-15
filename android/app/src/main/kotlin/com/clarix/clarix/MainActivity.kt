a91ff78104e1282dbc0044fe37ebfde41a598ec7
Elvis Fosu Owusu <esfokom@gmail.com>
2026-05-15 05:08:51 +0000

    Initial Commit

---
+++ android/app/src/main/kotlin/com/clarix/clarix/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.clarix.clarix
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ android/app/src/main/kotlin/com/example/clarix/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.clarix
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
