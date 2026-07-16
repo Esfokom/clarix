41a6421ad6fb576688160b8a7631ab03f9d805ef
Elvis Fosu Owusu <esfokom@gmail.com>
2026-07-16 16:27:46 +0000

    fix: import trackpad gesture events

--- lib/src/features/workspace/presentation/widgets/document_workspace.dart
+++ lib/src/features/workspace/presentation/widgets/document_workspace.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:pdfrx/pdfrx.dart';
