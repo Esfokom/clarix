9d06120b3fcea6b132eb3702dc9b2eaaa932ba80
Elvis Fosu Owusu <esfokom@gmail.com>
2026-07-28 13:51:30 +0000

    fix: initialize Rust bridge during bootstrap

--- lib/src/core/boot.dart
+++ lib/src/core/boot.dart
@@ -3,10 +3,12 @@ import 'package:pdfrx/pdfrx.dart';
 import 'package:window_manager/window_manager.dart';
 
 import 'clarix_logger.dart';
+import 'clarix_rust_runtime.dart';
 
 Future<void> bootstrapClarix() async {
   WidgetsFlutterBinding.ensureInitialized();
   clarixLog.i('Bootstrapping Clarix.');
+  await ClarixRustRuntime.ensureInitialized();
   await pdfrxFlutterInitialize();
   await windowManager.ensureInitialized();
   clarixLog.i('Clarix bootstrap complete.');
